8e37f3b40adfbce71abb3c9c56093d00ff6bbeb6
TabishRizvi <sayyidtabish@gmail.com>
2016-10-14 20:19:23 +0530

    Revert "test 9"
    This reverts commit a4e58df9c28a48f5c8c018f69ef0ff05384c189e.

--- test/test.js
+++ test/test.js
@@ -1,44 +1,45 @@
-var chai = require("chai");
-var expect = chai.expect;
-var straight = require("../index");
-var assert = require("assert");
 
-describe("Straight", function () {
+var chai = require('chai');
+var expect = chai.expect;
+var straight = require('../index');
+var assert = require('assert');
+
+describe('Straight', function() {
 
     this.timeout(15000);
 
-    it("series", function (done) {
+    it('series', function(done) {
         var callOrder = [];
         straight([
-            function (cb) {
-                setTimeout(function () {
+            function(cb){
+                setTimeout(function(){
                     callOrder.push(1);
-                    cb(null, "hello");
-                }, 1000);
+                    cb(null,"hello");
+                },1000);
             },
-            function (cb) {
-                setTimeout(function () {
+            function(cb){
+                setTimeout(function(){
                     callOrder.push(2);
-                    cb(null, "world");
-                }, 1000);
+                    cb(null,"world");
+                },1000);
             }
-        ], function (err, result) {
+        ],function(err,result){
             expect(err).to.eql(null);
-            expect(result).to.eql(["hello", "world"]);
-            expect(callOrder).to.eql([1, 2]);
+            expect(result).to.eql(["hello","world"]);
+            expect(callOrder).to.eql([1,2]);
             done();
         });
     });
 
-    it("empty array", function (done) {
-        straight([], function (err, results) {
+    it('empty array', function(done) {
+        straight([], function(err, results){
             expect(err).to.equal(null);
             expect(results).to.eql([]);
             done();
         });
     });
 
-    it("series object", function (done) {
+    it('series object', function(done) {
         straight({
             x: function (cb) {
                 setTimeout(function () {
@@ -50,14 +51,14 @@ describe("Straight", function () {
                     cb(null, "world");
                 }, 1000);
             }
-        }, function (err, result) {
+        },function(err,result){
             expect(err).to.eql(null);
-            expect(result).to.eql({x: "hello", y: "world"});
+            expect(result).to.eql({ x : "hello", y: "world"});
             done();
         });
     });
 
-    it("error", function (done) {
+    it('error', function(done) {
         straight({
             x: function (cb) {
                 setTimeout(function () {
@@ -69,44 +70,35 @@ describe("Straight", function () {
                     cb(null, "world");
                 }, 1000);
             }
-        }, function (err, result) {
+        },function(err,result){
             expect(err).to.eql("this is error");
             done();
         });
     });
 
-    it("no callback", function (done) {
+    it('no callback', function(done) {
         straight([
-            function (callback) {
-                callback();
-            },
-            function (callback) {
-                callback();
-                done();
-            }
+            function(callback){callback();},
+            function(callback){callback(); done();}
         ]);
     });
 
-    it("falsy return values", function (done) {
+    it('falsy return values', function(done) {
         function taskFalse(callback) {
             callback(null, false);
         }
-
         function taskUndefined(callback) {
             callback(null, undefined);
         }
-
         function taskEmpty(callback) {
             callback(null);
         }
-
         function taskNull(callback) {
             callback(null, null);
         }
-
         straight(
             [taskFalse, taskUndefined, taskEmpty, taskNull],
-            function (err, results) {
+            function(err, results) {
                 expect(results.length).to.equal(4);
                 assert.strictEqual(results[0], false);
                 assert.strictEqual(results[1], undefined);
@@ -117,41 +109,41 @@ describe("Straight", function () {
         );
     });
 
-    it("tasks not array or object", function () {
-        expect(straight.bind(straight, 12312, function (err, result) {
-        })).to.throw(Error, /Tasks must be an array or object./);
+    it('tasks not array or object', function() {
+        expect(straight.bind(straight,12312,function(err,result){
+        })).to.throw(Error,/Tasks must be an array or object./);
     });
 
-    it("tasks is null", function () {
-        expect(straight.bind(straight, null, function (err, result) {
-        })).to.throw(Error, /Tasks must be an array or object./);
+    it('tasks is null', function() {
+        expect(straight.bind(straight,null,function(err,result){
+        })).to.throw(Error,/Tasks must be an array or object./);
     });
 
-    it("tasks not all elements as functions", function () {
-        expect(straight.bind(straight, [
-            function (cb) {
-                setTimeout(function () {
-                    cb(null, "hello");
-                }, 1000);
+    it('tasks not all elements as functions', function() {
+        expect(straight.bind(straight,[
+            function(cb){
+                setTimeout(function(){
+                    cb(null,"hello");
+                },1000);
             },
             "this is not a function"
-        ], function (err, result) {
-        })).to.throw(Error, /Tasks must be an array of functions or object with values as functions./);
+        ],function(err,result){
+        })).to.throw(Error,/Tasks must be an array of functions or object with values as functions./);
     });
 
-    it("final callback is not function", function () {
-        expect(straight.bind(straight, [
-            function (cb) {
-                setTimeout(function () {
-                    cb(null, "hello");
-                }, 1000);
+    it('final callback is not function', function() {
+        expect(straight.bind(straight,[
+            function(cb){
+                setTimeout(function(){
+                    cb(null,"hello");
+                },1000);
             },
-            function (cb) {
-                setTimeout(function () {
-                    cb(null, "world");
-                }, 2000);
+            function(cb){
+                setTimeout(function(){
+                    cb(null,"world");
+                },2000);
             }
-        ], "this is not a function")).to.throw(Error, /Final callback, if supplied, must be a function./);
+        ],"this is not a function")).to.throw(Error,/Final callback, if supplied, must be a function./);
     });
 
 });
